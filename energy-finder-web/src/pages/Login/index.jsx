e9270ae44388ed15beff233aa0bed1ce62e0f801
alessandro raul <alessandro.guedes@bandtec.com.br>
2022-01-28 14:09:37 -0300

    feat: refactor in login and implements user register

--- energy-finder-web/src/pages/Login/index.jsx
+++ energy-finder-web/src/pages/Login/index.jsx
@@ -6,24 +6,62 @@ import api from "../../utils/api";
 function Login() {
     const [isRegister, setIsRegister] = useState(false);
     const [loginOrRegisterLabel, setLoginOrRegisterLabel] = useState('Realizar Login');
-    const [loginOrRegisterBtn, setLoginOrRegisterBtn] = useState('Não possuo cadastro');
+    const [loginOrRegisterBtn, setLoginOrRegisterBtn] = useState('Entrar');
+    const [loginOrRegisterSecondBtn, setLoginOrRegisterSecondBtn] = useState('Não possuo cadastro');
 
-    const[userEmail, setUserEmail] = useState('');
-    const[userPassword, setUserPassword] = useState('');
+    const [userName, setUserName] = useState('');
+    const [userEmail, setUserEmail] = useState('');
+    const [userPassword, setUserPassword] = useState('');
 
     useEffect(() => {
         if (isRegister) {
             setLoginOrRegisterLabel('Realizar Cadastro');
-            setLoginOrRegisterBtn('Já tenho cadastro');
+            setLoginOrRegisterBtn('Cadastrar');
+            setLoginOrRegisterSecondBtn('Já possuo cadastro');
         } else {
             setLoginOrRegisterLabel('Realizar Login');
-            setLoginOrRegisterBtn('Não tenho cadastro');
+            setLoginOrRegisterBtn('Entrar');
+            setLoginOrRegisterSecondBtn('Não possuo cadastro');
         }
     }, [isRegister]);
 
-    const auth = async function () {
-        const { data } = await api.get(`/user/auth/${userEmail}/${userPassword}`);
-        console.log(data);
+    const auth = async function (event) {
+        event.preventDefault();
+        try {
+            const { data } = await api.get(`/user/auth/${userEmail}/${userPassword}`);
+
+            if (data.auth) {
+                localStorage.setItem('@dataUser', JSON.stringify(data));
+                console.log(data);
+                alert('logado!');
+            } else {
+                alert('Erro na autenticação');
+            }
+        } catch (error) {
+            console.error(error.response.data);
+            alert(error.response.status);
+        }
+    }
+
+    const register = async function (event) {
+        event.preventDefault();
+
+        const userJson = {
+            username: userName,
+            email: userEmail,
+            password: userPassword
+        }
+
+        try {
+            const { data } = await api.post('/user', userJson);
+            if (data.success) {
+                alert('criado!');
+                setIsRegister(false);
+            }
+        } catch (error) {
+            console.error(error.response.data);
+            alert(error.response.status);
+        }
     }
 
     return (
@@ -38,15 +76,14 @@ function Login() {
             <section className="form-side">
                 <div className="form">
                     <p className="brand"><span>E</span>nergy <span>F</span>inder</p>
-                    <div className="inputs">
+                    <form className="inputs" onSubmit={e => isRegister ? register(e) : auth(e) }>
                         <p>{loginOrRegisterLabel}</p>
-                        {isRegister && <input type="text" placeholder="Nome" />}
-                        <input type="text" placeholder="Email" onChange={e => setUserEmail(e.target.value)}/>
-                        <input type="text" placeholder="Senha" onChange={e => setUserPassword(e.target.value)}/>
-                        {!isRegister && <button onClick={() => auth()}> Entrar </button>}
-                        {isRegister && <button> Cadastrar </button>}
-                        <p onClick={() => setIsRegister(!isRegister)}>{loginOrRegisterBtn}</p>
-                    </div>
+                        {isRegister && <input type="text" required placeholder="Nome" onChange={e => setUserName(e.target.value)} />}
+                        <input type="email" required placeholder="Email" onChange={e => setUserEmail(e.target.value)} />
+                        <input type="password" required placeholder="Senha" onChange={e => setUserPassword(e.target.value)} />
+                        <button type="submit"> {loginOrRegisterBtn} </button>
+                        <p onClick={() => setIsRegister(!isRegister)}>{loginOrRegisterSecondBtn}</p>
+                    </form>
                 </div>
             </section>
         </section>
