e42d8d0ae365c216d0a7db75d8829584ed4dcd1a
alessandro raul <alessandro.guedes@bandtec.com.br>
2022-01-29 00:09:09 -0300

    feat: add snackbar alerts in login screen

--- energy-finder-web/src/pages/Login/index.jsx
+++ energy-finder-web/src/pages/Login/index.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 import './style.css';
 import '../global.css';
 import api from "../../utils/api";
+import { height } from "@mui/system";
 
 function Login() {
     const navigate = useNavigate();
@@ -16,6 +19,8 @@ function Login() {
     const [userPassword, setUserPassword] = useState('');
     const [token, setToken] = useState(localStorage.getItem('token'));
     const [loading, setLoading] = useState(false);
+    const [openAlert, setOpenAlert] = useState(false);
+    const [alertMessage, setAlertMessage] = useState('');
 
     useEffect(() => {
         if (isRegister) {
@@ -40,6 +45,10 @@ function Login() {
         }
     }
 
+    const Alert = React.forwardRef(function Alert(props, ref) {
+        return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+    });
+
     const auth = async function (event) {
         event.preventDefault();
         setLoading(true);
@@ -50,13 +59,18 @@ function Login() {
                 localStorage.setItem('token', data.token);
                 navigate('/home');
             } else {
-                alert('Erro na autenticação');
+                setAlertMessage('Erro ao gerar token de autenticação');
             }
         } catch (error) {
-            console.error(error.response.data);
-            alert(error.response.status);
+            console.error(error.response.status);
+            if (error.response.status = 404) {
+                setAlertMessage('Usuário não encontrado');
+            } else {
+                setAlertMessage('Erro ao realizar login');
+            }
         }
         setLoading(false);
+        setOpenAlert(true);
     }
 
     const register = async function (event) {
@@ -71,46 +85,58 @@ function Login() {
         try {
             const { data } = await api.post('/user', userJson);
             if (data.success) {
-                alert('criado!');
+                setAlertMessage('Cadastrado com sucesso!');
                 setIsRegister(false);
             }
         } catch (error) {
-            console.error(error.response.data);
-            alert(error.response.status);
+            console.error(error.response);
+            setAlertMessage('Erro ao realizar cadastro');
         }
         setLoading(false);
+        setOpenAlert(true);
     }
 
     return (
-        <section>
-            <section className="canva-side">
-                <p>Fornecedor de energia ideal? só com o
-                    <span className="brand-span">
-                        <span> E</span>nergy <span>F</span>inder
-                    </span>.
-                </p>
+        <>
+            <section>
+                <section className="canva-side">
+                    <p>Fornecedor de energia ideal? só com o
+                        <span className="brand-span">
+                            <span> E</span>nergy <span>F</span>inder
+                        </span>.
+                    </p>
+                </section>
+                <section className="form-side">
+                    <div className="form">
+                        <p className="brand"><span>E</span>nergy <span>F</span>inder</p>
+                        <form className="inputs" onSubmit={e => isRegister ? register(e) : auth(e)}>
+                            <p>{loginOrRegisterLabel}</p>
+                            {isRegister && <input type="text" required placeholder="Nome" onChange={e => setUserName(e.target.value)} />}
+                            <input type="email" required placeholder="Email" onChange={e => setUserEmail(e.target.value)} />
+                            <input type="password" required placeholder="Senha" onChange={e => setUserPassword(e.target.value)} />
+                            {!loading &&
+                                <>
+                                    <button type="submit"> {loginOrRegisterBtn} </button>
+                                    <p onClick={() => setIsRegister(!isRegister)}>{loginOrRegisterSecondBtn}</p>
+                                </>
+                            }
+                            {loading &&
+                                <div className="loader"></div>
+                            }
+                        </form>
+                    </div>
+                </section>
+
             </section>
-            <section className="form-side">
-                <div className="form">
-                    <p className="brand"><span>E</span>nergy <span>F</span>inder</p>
-                    <form className="inputs" onSubmit={e => isRegister ? register(e) : auth(e)}>
-                        <p>{loginOrRegisterLabel}</p>
-                        {isRegister && <input type="text" required placeholder="Nome" onChange={e => setUserName(e.target.value)} />}
-                        <input type="email" required placeholder="Email" onChange={e => setUserEmail(e.target.value)} />
-                        <input type="password" required placeholder="Senha" onChange={e => setUserPassword(e.target.value)} />
-                        {!loading &&
-                            <>
-                                <button type="submit"> {loginOrRegisterBtn} </button>
-                                <p onClick={() => setIsRegister(!isRegister)}>{loginOrRegisterSecondBtn}</p>
-                            </>
-                        }
-                        {loading &&
-                            <div className="loader"></div>
-                        }
-                    </form>
-                </div>
-            </section>
-        </section>
+            <Snackbar open={openAlert}
+                autoHideDuration={6000}
+                onClose={() => setOpenAlert(false)}
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}>
+                <Alert onClose={() => setOpenAlert(false)} severity={alertMessage.includes('sucesso') ? 'success' : 'error'}>
+                    {alertMessage}
+                </Alert>
+            </Snackbar>
+        </>
     )
 }
 
